db0b3cf51dba3141fb73a77b437afe8d966c7da5
vashukyadav <vashuuyadav08@gmail.com>
2025-10-09 14:22:54 +0530

    Update backend: comment out React app serving code and add .env file

--- server.js
+++ server.js
@@ -207,12 +207,12 @@ app.delete('/api/admin/contacts/:id', authenticateAdmin, (req, res) => {
 });
 
 // Serve React app in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
